ce59761fb70d45f517972491bbfcd8d704849af6
kevin bagley <bagmister@yahoo.com>
2025-08-06 23:43:06 -0600

    updates

--- bird-app/scripts/externalSources.js
+++ bird-app/scripts/externalSources.js
@@ -1,27 +1,8 @@
 const apiKeyParks = import.meta.env.apiKeyParks
 const apiKeyBirds = import.meta.env.apiKeyBirds
 
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 export default class ExternalServices {
   constructor() {
 
@@ -46,7 +27,6 @@ export default class ExternalServices {
         'X-Api-Key': apiKey
       })
     });
-
 
     fetch(request)
       .then(function () {
---
+++ bird-app/scripts/utils.mjs
@@ -0,0 +1,66 @@
+export function qs(selector, parent = document) {
+  return parent.querySelector(selector);
+}
+
+export function getLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+export function setLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+export function setClick(selector, callback) {
+  qs(selector).addEventListener("touchend", (event) => {
+    event.preventDefault();
+    callback();
+  });
+  qs(selector).addEventListener("click", callback);
+}
+
+export function loadpageSection(key, partialFilePath) {
+  let domSelector = null;
+  let path = "";
+  if (key === 1) {
+    domSelector = document.querySelector(".footerForPage");
+    path = `${partialFilePath}/footer.html`;
+  } else if (key === 0) {
+    domSelector = document.querySelector(".headerForPage");
+    path = `${partialFilePath}/header.html`;
+  } else {
+    console.error("No page section to load for key: ", key);
+    return Promise.resolve();
+  }
+  if (!domSelector) {
+    console.error(`DOM selector for key ${key} not found.`);
+    return Promise.resolve();
+  }
+  return renderPage(domSelector, path);
+}
+
+async function renderPage(domSelector, path) {
+  try {
+    const htmlContent = await getHtmlContent(path);
+    if (htmlContent) {
+      domSelector.innerHTML = htmlContent;
+    } else {
+      console.error(`No content loaded for path: ${path}`);
+      domSelector.innerHTML = "<p>Error loading content</p>";
+    }
+  } catch (error) {
+    console.error(`Error rendering page section for path: ${path}`, error);
+    domSelector.innerHTML = "<p>Error loading content</p>";
+  }
+}
+
+async function getHtmlContent(path) {
+  try {
+    const fileResponse = await fetch(path);
+    if (!fileResponse.ok) {
+      throw new Error(`Failed to fetch path: ${path}, Status: ${fileResponse.status}`);
+    }
+    const content = await fileResponse.text();
+    return content;
+  } catch (error) {
+    console.error("Error fetching content: ", path, error);
+    return null;
+  }
+}
