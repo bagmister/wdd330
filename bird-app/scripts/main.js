c9d57d3a2c0c1f62f87f37accaf178c72acbd85d
kevin bagley <bagmister@yahoo.com>
2025-08-09 16:41:12 -0600

    updates

--- bird-app/scripts/main.js
+++ bird-app/scripts/main.js
@@ -1,10 +1,15 @@
 import { loadpageSection } from "./utils.mjs";
 const partialFilePath = "/partials";
+const headerContainer = document.querySelector(".headerForPage");
+const footerContainer = document.querySelector(".footerForPage");
+headerContainer.innerHTML = "";
+footerContainer.innerHTML = "";
 
 document.addEventListener("DOMContentLoaded", () => {
-  loadpageSection(0, partialFilePath);
-  loadpageSection(1, partialFilePath);
-   fetch('map.html')
+
+    loadpageSection(0, partialFilePath);
+    loadpageSection(1, partialFilePath);
+    fetch('map.html')
         .then(response => {
             if (!response.ok) throw new Error('Failed to load map partial');
             return response.text();
